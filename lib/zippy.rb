097bba6ae791f7d9c03998ba400813f979e7c303
Philip Potter <philipgpotter@gmail.com>
2013-02-25 22:38:47 +0000

    remove Zippy::Define

--- lib/zippy.rb
+++ lib/zippy.rb
@@ -1,26 +1,6 @@
 require 'puppet'
 
 module Zippy
-  class Define
-    def initialize(type,code)
-      @type = type
-      @code = code
-    end
-
-    def instantiate(title)
-      Puppet[:manifestdir] = RSpec.configuration.manifest_dir
-      Puppet[:modulepath] = RSpec.configuration.module_path
-      compiler = Puppet::Parser::Compiler.new(Puppet::Node.new('localhost'))
-      parser = Puppet::Parser::Parser.new compiler.environment.name
-      #FIXME what should the module name be? probably not jimmy
-      compiler.known_resource_types.import_ast(parser.parse(@code),'jimmy')
-      ast = parser.parse "foo{'#{title}':}"
-      resources = ast.code[0].evaluate(compiler.topscope)
-      resources[0].evaluate
-      compiler.catalog.resources
-    end
-  end
-
   def self.instantiate(code)
     Puppet[:manifestdir] = RSpec.configuration.manifest_dir
     Puppet[:modulepath] = RSpec.configuration.module_path
--- spec/zippy_spec.rb
+++ spec/zippy_spec.rb
@@ -1,21 +1,6 @@
 require 'spec_helper'
 require 'zippy'
 
-describe Zippy::Define do
-  it 'should test a define' do
-    define_code = <<END
-define foo () {
-  file {"/etc/${title}":
-    ensure  => present,
-    content => "bazinga",
-  }
-}
-END
-    define = Zippy::Define.new('foo',define_code)
-    define.instantiate('foobar').should contain_file('/etc/foobar')
-  end
-end
-
 describe Zippy do
   it 'should test a define from a module' do
     instance_code = %q[nginx::site {'foo':}]
