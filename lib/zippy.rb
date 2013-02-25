3cfd8b8c67a5b9e0003b9206858d7bd35ffbb674
Philip Potter <philipgpotter@gmail.com>
2013-02-25 22:40:55 +0000

    split matchers into separate file

--- lib/zippy.rb
+++ lib/zippy.rb
@@ -1,4 +1,5 @@
 require 'puppet'
+require 'zippy/matchers'
 
 module Zippy
   def self.instantiate(code)
@@ -12,59 +13,6 @@ module Zippy
     compiler.catalog.resources
   end
 
-  module CatalogMatchers
-    extend RSpec::Matchers::DSL
-
-    class CatalogMatcher
-      #    matcher :contain_file do |expected_title|
-      def initialize(expected_type,expected_title)
-        @expected_type = expected_type
-        @expected_title = expected_title
-        @referenced_type = expected_type.capitalize
-      end
-
-      def matches?(actual)
-        resource = actual.find do |resource|
-          resource.type == @referenced_type && resource.name == @expected_title
-        end
-        if resource then
-          (@expected_params || {}).all? do |name,expected_val|
-            resource[name] == expected_val
-          end
-        else
-          false
-        end
-      end
-
-      def failure_message_for_should
-        param_string = ""
-        if @expected_params
-          param_string = " with parameters #{@expected_params.inspect}"
-        end
-        "expected the catalog to contain #{@referenced_type}[#{@expected_title}]#{param_string}"
-      end
-
-      def method_missing(method, *args, &block)
-        if method.to_s =~ /^with_/
-          param = method.to_s.gsub(/^with_/,'')
-          @expected_params ||= {}
-          @expected_params[param] = args[0]
-          self
-        else
-          super
-        end
-      end
-    end
-
-    def method_missing(method, *args, &block)
-      if method.to_s =~ /^contain_/
-        resource_type = method.to_s.gsub(/^contain_/,'')
-        CatalogMatcher.new(resource_type,args[0])
-      else
-        super
-      end
-    end
-  end
 end
 
 RSpec.configure do |c|
