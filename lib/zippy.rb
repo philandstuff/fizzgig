bd0d5776350078ef684f0a61de12d98a218cc61e
Philip Potter <philipgpotter@gmail.com>
2013-02-26 20:39:05 +0000

    don't include Zippy::CatalogMatchers everywhere by default

--- lib/zippy.rb
+++ lib/zippy.rb
@@ -20,7 +20,6 @@ RSpec.configure do |c|
   c.add_setting :module_path, :default => '/etc/puppet/modules'
   c.add_setting :manifest_dir, :default => nil
 
-  c.include Zippy::CatalogMatchers
   # FIXME: decide if these are needed
   #c.add_setting :manifest, :default => nil
   #c.add_setting :template_dir, :default => nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,6 @@ HERE = File.expand_path(File.dirname(__FILE__))
 RSpec.configure do |c|
   c.module_path  = File.join(HERE, 'modules')
   c.manifest_dir = File.join(HERE, 'manifests')
+
+  c.include Zippy::CatalogMatchers
 end
