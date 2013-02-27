c39127539021078331720d90666e4efb6a288dc3
Philip Potter <philipgpotter@gmail.com>
2013-02-27 21:12:31 +0000

    extract FunctionStubs module

--- lib/zippy.rb
+++ lib/zippy.rb
@@ -1,5 +1,6 @@
 require 'puppet'
 require 'zippy/matchers'
+require 'zippy/function_stubs'
 
 module Zippy
   def self.instantiate(code)
@@ -28,41 +29,6 @@ module Zippy
     ast = parser.parse("include #{klass}")
     resources = ast.code[0].evaluate(scope)
     compiler.catalog
-  end
-
-  module FunctionStubs
-    def self.has_stub?(fname,args)
-      spec = Thread.current[:spec]
-      spec &&
-        spec.respond_to?(:function_stubs) &&
-        spec.function_stubs.has_key?(fname.to_sym) &&
-        spec.function_stubs[fname.to_sym][args[0]]
-    end
-
-    def self.get_stub(fname,args)
-      spec = Thread.current[:spec]
-      spec.function_stubs[fname.to_sym][args[0]]
-    end
-  end
-end
-
-module Puppet::Parser::Functions
-  class << self
-    alias_method :real_newfunction, :newfunction
-    def newfunction(name, options = {}, &block)
-      func = real_newfunction(name, options, &block)
-      real_fname = "real_function_#{name}"
-      orig_fname = "orig_function_#{name}"
-      environment_module.send(:alias_method, orig_fname.to_sym, real_fname.to_sym)
-      environment_module.send(:define_method, real_fname) do |args|
-        if Zippy::FunctionStubs.has_stub?(name,args)
-          Zippy::FunctionStubs.get_stub(name,args)
-        else
-          self.send(orig_fname, args)
-        end
-      end
-      func
-    end
   end
 end
 
