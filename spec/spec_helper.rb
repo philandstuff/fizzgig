12a1299d510c440e0d3ec93a0e036500c7a7c6cd
Philip Potter <philip.g.potter@gmail.com>
2013-03-25 21:38:16 +0000

    ensure that modulepath can take multiple directories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'fizzgig'
 HERE = File.expand_path(File.dirname(__FILE__))
 
 RSpec.configure do |c|
-  c.modulepath  = File.join(HERE, 'modules')
+  c.modulepath  = "#{File.join(HERE, 'modules')}:#{File.join(HERE, 'extra_modules')}"
 
   c.include Fizzgig::CatalogMatchers
 end
